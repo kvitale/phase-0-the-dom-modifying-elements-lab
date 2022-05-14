ff25543b08a98b8c7196c4040b271dc2810f6287
Kyle <ktvitale7@gmail.com>
2022-05-14 13:28:46 -0400

    commit tree a38fee3a9e05dea0b46ef501315446f55ee316e9

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const deleteDomNode = main.remove()
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+
+ newHeader.innerHTML = "Kyle is the champion"
